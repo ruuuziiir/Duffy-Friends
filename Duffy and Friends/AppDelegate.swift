d342278d405acc68fd22f3e91d18ac8494cc9a1d
7 <ruuuziiir@gmail.com>
2020-11-15 10:26:54 +0800

    duffy and friends

--- Duffy and Friends/AppDelegate.swift
+++ Duffy and Friends/AppDelegate.swift
@@ -14,6 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+       
+        let barAppearance =  UINavigationBarAppearance()
+                         
+        barAppearance.configureWithTransparentBackground()
+        UINavigationBar.appearance().standardAppearance = barAppearance
+        
+        let image = UIImage()
+          UITabBar.appearance().backgroundImage = image
+          UITabBar.appearance().shadowImage = image
+        
         return true
     }
 
@@ -31,6 +41,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
-}
+  }
 
--- Duffy and Friends/ViewController.swift
+++ Duffy and Friends/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        try! FileManager.default.removeItem(atPath: NSHomeDirectory()+"/Library/SplashBoard")
+
+        do {
+            sleep(10)
+        }
+
+        return true
+    }
 
 }
 
